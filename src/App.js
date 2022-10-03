e6f58f29a1b0faff77320eba6ca8b5f064e97c8e
Ashwini Gupta <ashwinigupta8052653693@gmail.com>
2022-10-03 21:03:37 +0530

    Initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+// let nam = "jyotika"
+function App() {
+  return (
+    // JSX
+    <>
+     
+      <Navbar title="1" aboutText="About TextUtils"/>
+
+    </>
+  );
+}
+export default App;
